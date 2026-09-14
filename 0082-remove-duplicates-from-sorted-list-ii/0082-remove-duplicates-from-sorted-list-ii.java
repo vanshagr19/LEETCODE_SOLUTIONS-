75dc5b872ef4476d393ae4fc71352132550ddd67
Vansh Agrawal <vansh1909agrawal@gmail.com>
2026-09-15 01:14:02 +0530

    Time: 0 ms (100%), Space: 44.8 MB (86.64%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -1,31 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) return head;
+        ListNode dummy =new ListNode(-1);
+        ListNode temp =dummy;
+        ListNode i =head;
+        ListNode j =head;
         
-        ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
-        ListNode i = head;
-        ListNode j = head;
-        
-        while (j != null) {
-            int cnt = 0;
-            
-            // FIX: Check j != null instead of j.next != null to process the tail safely
-            while (j != null && i.val == j.val) {
-                cnt++;
+        if (head ==null|| head.next == null) return head;
+        while(j != null){
+            int cnt =0;
+            while(  j != null && i.val == j.val){
+                cnt ++;
                 j = j.next;
             }
-            
-            if (cnt > 1) {
-                i = j; // Skip duplicates
-            } else {
+            if (cnt>1){
+                i=j;
+                
+            }
+            else{
                 temp.next = i;
                 temp = i;
-                i = j; // Advance i to j
+                i =j;
             }
         }
-        
-        temp.next = null; // Cut off trailing references
+
+        temp.next =null;
         return dummy.next;
     }
 }
